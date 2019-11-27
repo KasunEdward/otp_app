43fa5c3cbd2be058584ee8c166469f581f9784ba
Kasun <kasun@omobio.net>
2019-11-27 22:03:34 +0530

    Added OTP APP initial code
    mix new otp_app --sup

---
+++ lib/otp_app/application.ex
@@ -0,0 +1,19 @@
+defmodule OtpApp.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: OtpApp.Worker.start_link(arg)
+      # {OtpApp.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: OtpApp.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
